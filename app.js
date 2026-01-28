ec95829ddbf80725d9f783f592aaa931c0173080
AngelB25 <a.boissie@lyon.ort.asso.fr>
2026-01-28 11:47:08 +0100

    lecture de json

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const users = require('./modules/users')
 
-// Route a la racine renvoyant un message
-app.get("/", (req, res) => {  
-res.json( 
-{  
-message : "Bienvenue sur l'API TCG",  
-data : {} 
-} 
-); 
-}); 
+app.use(express.urlencoded({ extended: true }));
 
-// gérer les données des formulaires
-app.use(express.urlencoded({ extended: true })); 
+app.get("/", (req, res) => {
+    res.json({
+        message: "Bienvenue sur l'API TCG",
+        data: {}
+    });
+});
 
-// Démarrer le serveur
+app.post("/register", users.RegisterUser)
+
 app.listen(port, () => {
-  console.log(`Serveur démarré sur http://localhost:${port}`);
+    console.log(`Serveur démarré sur http://localhost:${port}`);
 });
--- modules/users.js
+++ modules/users.js
@@ -1,6 +1,22 @@
-const express = require('express');
+const fs = require('fs');
 
-function RegisterUser(req, res){
-    
+function RegisterUser(req, res) {
+    if (!req.body) {
+        return res.status(400).json({ message: "Erreur : Aucune donnée" });
+    }
+
+    const { username, password, collection } = req.body;
+
+    // lire le json des utilisateurs
+    try {
+        const data = fs.readFileSync('./data/users.json', 'utf8');
+
+        console.log(data)
+
+        res.json({ message: "OK" });
+    } catch (err) {
+        console.error("erreur");
+    }
 }
-app.post("/register", users.RegisterUser);
+
+module.exports = { RegisterUser };
