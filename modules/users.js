5e969f5af586bb0e2e9f20acb12702436184406b
AngelB25 <a.boissie@lyon.ort.asso.fr>
2026-01-28 09:37:33 +0100

    ignore

--- modules/users.js
+++ modules/users.js
@@ -1,7 +1,5 @@
 const express = require('express');
 
-// Données simulées
-let users = [
-  { id: 1, username: 'Alice', password: 'pass5678' },
-  { id: 2, username: 'Bob', password: 'pass1234' }
-];
+function RegisterUser(req, res){
+    
+}
